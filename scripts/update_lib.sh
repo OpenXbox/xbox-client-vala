e7bc9eb05594791ee9cbbd35909fe2fb3b4fb63b
Gabriel Fróes Franco <gffranco@gmail.com>
2019-04-24 22:19:27 +0200

    removing unused scripts

--- scripts/update_lib.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cp ~/git-repositories/xbox-webapi-vala/build/XboxWebApi.h ../lib/.
-cp ~/git-repositories/xbox-webapi-vala/build/libXboxWebApi.so ../lib/.
-cp ~/git-repositories/xbox-webapi-vala/build/XboxWebApi-1.0.vapi ../data/vapi/.
-
